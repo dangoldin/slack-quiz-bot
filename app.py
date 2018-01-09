4de27cc263bb1c2a498d9dee78680e5ec0c25488
Dan Goldin <dgoldin@triplelift.com>
2018-01-08 22:52:22 -0500

    Got basics working

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 #! /usr/bin/env python
 
+import os
+
+from sheet_helper import GSheetHelper
+from slack_helper import SlackHelper
+
 from flask import Flask
 app = Flask(__name__)
 
+gs = GSheetHelper(os.environ['CREDENTIALS_FILE'])
+sh = SlackHelper(os.environ['SLACK_TOKEN'])
+
 @app.route('/')
 def index():
-    return 'App works!'
+    questions = gs.get_rows('Quiz questions', 'Questions')
+    return str(questions)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
