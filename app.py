9065700d191d6ee62a7623a837f0a3ae15fee130
Dan Goldin <dgoldin@triplelift.com>
2018-01-10 23:06:03 -0500

    Tiny bit more work

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
 import os
+import random
+import json
 
 from sheet_helper import GSheetHelper
 from slack_helper import SlackHelper
@@ -11,10 +13,25 @@ app = Flask(__name__)
 gs = GSheetHelper(os.environ['CREDENTIALS_FILE'])
 sh = SlackHelper(os.environ['SLACK_TOKEN'])
 
+def get_questions():
+    return gs.get_rows('Quiz questions', 'Questions')
+
+@app.route('/quizme')
+def quizme():
+    questions = get_questions()
+    question = random.choice(questions)
+    answer = question['Answer']
+    choices = [question[x] for x in ('Choice A', 'Choice B', 'Choice C')]
+    return json.dumps({
+        'question': question['Question'],
+        'answer': answer,
+        'choices': choices,
+    })
+
 @app.route('/')
 def index():
-    questions = gs.get_rows('Quiz questions', 'Questions')
-    return str(questions)
+    questions = get_questions()
+    return json.dumps(questions)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
